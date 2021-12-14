4d631659c8b2ceff612cc61b43bccfd84e0ed4b2
Will Morris <teamexangus@gmail.com>
2021-12-14 13:14:44 +1100

    add SyntaxNode.WriteTo

--- Peach/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Peach/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,53 @@ namespace Peach.CodeAnalysis.Syntax
                 var last = GetChildren().Last().Span;
                 return TextSpan.FromBounds(first.Start, last.End);
             }
+        }
+
+        public void WriteTo(TextWriter writer)
+        {
+            PrettyPrint(writer, this);
+        }
+
+        private static void PrettyPrint(TextWriter writer, SyntaxNode node, string indent = "", bool isLast = true)
+        {
+            var marker = isLast ? "└──" : "├──";
+
+            writer.Write(indent);
+            writer.Write(marker);
+            writer.Write(node.Kind);
+
+            if (node is SyntaxToken t)
+            {
+                if (t.Value is not null)
+                {
+                    writer.Write(" ");
+                    writer.Write(t.Value);
+                }
+
+                if (t.Kind == SyntaxKind.IdentifierToken)
+                {
+                    writer.Write(" ");
+                    writer.Write(t.Text);
+                }
+            }
+
+            writer.WriteLine();
+
+            indent += isLast ? "   " : "│  ";
+
+            var lastChild = node.GetChildren().LastOrDefault();
+
+            foreach (var child in node.GetChildren())
+            {
+                PrettyPrint(writer, child, indent, child == lastChild);
+            }
+        }
+
+        public override string ToString()
+        {
+            var stringWriter = new StringWriter();
+            WriteTo(stringWriter);
+            return stringWriter.ToString();
         }
     }
 }
